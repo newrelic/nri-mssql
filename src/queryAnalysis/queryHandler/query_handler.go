07ab6f2b50ce0d3c0d265c69a7e235e7a39e41e7
Abhinav Tiwari <atiwari@newrelic.com>
2024-11-30 02:12:13 +0530

    remove commented code

--- src/queryAnalysis/queryHandler/query_handler.go
+++ src/queryAnalysis/queryHandler/query_handler.go
@@ -7,6 +7,7 @@ import (
 )
 
 type QueryHandler interface {
+	BindQueryResults(rows *sqlx.Rows, queryDetailsDto models.QueryDetailsDto) ([]interface{}, error)
 	LoadQueries() ([]models.QueryDetailsDto, error)
 	ExecuteQuery(db *sqlx.DB, queryConfig models.QueryDetailsDto) ([]interface{}, error)
 	IngestQueryMetrics(entity *integration.Entity, results []interface{}, queryDetailsDto models.QueryDetailsDto) error
