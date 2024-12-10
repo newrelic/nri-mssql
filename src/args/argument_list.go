eb7acc2665489e490e2b6b0eb2b137bbe9b62266
Abhinav Tiwari <atiwari@newrelic.com>
2024-12-10 20:59:40 +0530

    add new variables in the argumentList

--- src/args/argument_list.go
+++ src/args/argument_list.go
@@ -12,23 +12,26 @@ import (
 // ArgumentList struct that holds all MSSQL arguments
 type ArgumentList struct {
 	sdkArgs.DefaultArgumentList
-	Username                     string `default:"" help:"The Microsoft SQL Server connection user name"`
-	Password                     string `default:"" help:"The Microsoft SQL Server connection password"`
-	Instance                     string `default:"" help:"The Microsoft SQL Server instance to connect to"`
-	Hostname                     string `default:"127.0.0.1" help:"The Microsoft SQL Server connection host name"`
-	Port                         string `default:"" help:"The Microsoft SQL Server port to connect to. Only needed when instance not specified"`
-	EnableSSL                    bool   `default:"false" help:"If true will use SSL encryption, false will not use encryption"`
-	TrustServerCertificate       bool   `default:"false" help:"If true server certificate is not verified for SSL. If false certificate will be verified against supplied certificate"`
-	CertificateLocation          string `default:"" help:"Certificate file to verify SSL encryption against"`
-	EnableBufferMetrics          bool   `default:"true" help:"Enable collection of buffer space metrics."`
-	EnableDatabaseReserveMetrics bool   `default:"true" help:"Enable collection of database reserve space metrics."`
-	Timeout                      string `default:"30" help:"Timeout in seconds for a single SQL Query. Set 0 for no timeout"`
-	CustomMetricsQuery           string `default:"" help:"A SQL query to collect custom metrics. Query results 'metric_name', 'metric_value', and 'metric_type' have special meanings"`
-	CustomMetricsConfig          string `default:"" help:"YAML configuration with one or more SQL queries to collect custom metrics"`
-	ShowVersion                  bool   `default:"false" help:"Print build information and exit"`
-	ExtraConnectionURLArgs       string `default:"" help:"Appends additional parameters to connection url. Ex. 'applicationintent=readonly&foo=bar'"`
-	EnableDiskMetricsInBytes     bool   `default:"true" help:"Enable collection of instance.diskInBytes."`
-	EnableQueryPerformance       bool   `default:"true"`
+	Username                     string  `default:"" help:"The Microsoft SQL Server connection user name"`
+	Password                     string  `default:"" help:"The Microsoft SQL Server connection password"`
+	Instance                     string  `default:"" help:"The Microsoft SQL Server instance to connect to"`
+	Hostname                     string  `default:"127.0.0.1" help:"The Microsoft SQL Server connection host name"`
+	Port                         string  `default:"" help:"The Microsoft SQL Server port to connect to. Only needed when instance not specified"`
+	EnableSSL                    bool    `default:"false" help:"If true will use SSL encryption, false will not use encryption"`
+	TrustServerCertificate       bool    `default:"false" help:"If true server certificate is not verified for SSL. If false certificate will be verified against supplied certificate"`
+	CertificateLocation          string  `default:"" help:"Certificate file to verify SSL encryption against"`
+	EnableBufferMetrics          bool    `default:"true" help:"Enable collection of buffer space metrics."`
+	EnableDatabaseReserveMetrics bool    `default:"true" help:"Enable collection of database reserve space metrics."`
+	Timeout                      string  `default:"30" help:"Timeout in seconds for a single SQL Query. Set 0 for no timeout"`
+	CustomMetricsQuery           string  `default:"" help:"A SQL query to collect custom metrics. Query results 'metric_name', 'metric_value', and 'metric_type' have special meanings"`
+	CustomMetricsConfig          string  `default:"" help:"YAML configuration with one or more SQL queries to collect custom metrics"`
+	ShowVersion                  bool    `default:"false" help:"Print build information and exit"`
+	ExtraConnectionURLArgs       string  `default:"" help:"Appends additional parameters to connection url. Ex. 'applicationintent=readonly&foo=bar'"`
+	EnableDiskMetricsInBytes     bool    `default:"true" help:"Enable collection of instance.diskInBytes."`
+	EnableQueryPerformance       bool    `default:"true"`
+	SlowQueryThreshold           float32 `default:"500"`
+	SlowQueryCount               int     `default:"10"`
+	FetchInterval                int     `default:"15"`
 }
 
 // Validate validates SQL specific arguments
