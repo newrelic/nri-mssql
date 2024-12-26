e714eec720e9af48618869acddafc26315025c50
Abhinav Tiwari <atiwari@newrelic.com>
2024-12-26 12:34:28 +0530

    change custom error handling

--- src/queryAnalysis/models/customTypes.go
+++ src/queryAnalysis/models/customTypes.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"encoding/hex"
+	"errors"
 	"fmt"
 )
 
@@ -9,11 +10,17 @@ import (
 type HexString string
 type VarBinary64 string
 
+// Define static errors
+var (
+	ErrExpectedUint8Slice = errors.New("HexString: expected []uint8")
+	ErrExpectedByteSlice  = errors.New("VarBinary64: expected []byte")
+)
+
 // Scan implements the sql.Scanner interface for HexString
 func (h *HexString) Scan(value interface{}) error {
 	bytes, ok := value.([]uint8)
 	if !ok {
-		return fmt.Errorf("HexString: expected []uint8, got %T", value)
+		return fmt.Errorf("%w, got %T", ErrExpectedUint8Slice, value)
 	}
 
 	hexString := "0x" + hex.EncodeToString(bytes)
@@ -25,7 +32,7 @@ func (h *HexString) Scan(value interface{}) error {
 func (vb *VarBinary64) Scan(value interface{}) error {
 	bytes, ok := value.([]byte) // SQL drivers often use []byte for varbinary
 	if !ok {
-		return fmt.Errorf("VarBinary64: expected []byte, got %T", value)
+		return fmt.Errorf("%w, got %T", ErrExpectedByteSlice, value)
 	}
 
 	// Convert the []byte to a hex string with "0x" prefix
