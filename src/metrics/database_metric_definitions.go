aa8ff61ea992712a071dd3968bb896c4b1f3d3ef
Rahul Reddy <rannareddy@newrelic.com>
2025-06-06 13:05:29 +0530

    feat: add new database definition queries for azure sql database (#227)
    feat: add new database definition queries for azure sql database (#227)

--- src/metrics/database_metric_definitions.go
+++ src/metrics/database_metric_definitions.go
@@ -66,6 +66,69 @@ var databaseBufferDefinitions = []*QueryDefinition{
 	},
 }
 
+//nolint:all
+var specificDatabaseDefinitionsForAzureSQLDatabase = []*QueryDefinition{
+	{
+		query: `
+			SELECT 
+			    sd.name AS db_name,
+			    spc.cntr_value AS log_growth
+			FROM sys.dm_os_performance_counters spc
+			INNER JOIN sys.databases sd 
+			    ON sd.physical_database_name = spc.instance_name
+			WHERE spc.counter_name = 'Log Growths'
+			    AND spc.object_name LIKE '%:Databases%'
+			    AND sd.database_id = DB_ID()
+		`,
+		dataModels: &[]struct {
+			database.DataModel
+			LogGrowth int `db:"log_growth" metric_name:"log.transactionGrowth" source_type:"gauge"`
+		}{},
+	},
+	{
+		query: `		
+			SELECT
+			    DB_NAME() AS db_name,
+			    SUM(io_stall) AS io_stalls
+			FROM sys.dm_io_virtual_file_stats(NULL, NULL)
+			WHERE database_id = DB_ID()
+		`,
+		dataModels: &[]struct {
+			database.DataModel
+			IOStalls int `db:"io_stalls" metric_name:"io.stallInMilliseconds" source_type:"gauge"`
+		}{},
+	},
+	{
+		query: `
+			SELECT 
+			    DB_NAME() AS db_name, 
+			    COUNT_BIG(*) * (8 * 1024) AS buffer_pool_size
+			FROM sys.dm_os_buffer_descriptors WITH (NOLOCK) 
+			WHERE database_id = DB_ID()
+		`,
+		dataModels: &[]struct {
+			database.DataModel
+			BufferPoolSize int `db:"buffer_pool_size" metric_name:"bufferpool.sizePerDatabaseInBytes" source_type:"gauge"`
+		}{},
+	},
+	{
+		query: `
+			SELECT
+				DB_NAME() AS db_name,
+				sum(a.total_pages) * 8.0 * 1024 AS reserved_space,
+				(sum(a.total_pages)*8.0 - sum(a.used_pages)*8.0) * 1024 AS reserved_space_not_used
+			FROM sys.partitions p with (nolock)
+			INNER JOIN sys.allocation_units a WITH (NOLOCK) ON p.partition_id = a.container_id
+			LEFT JOIN sys.internal_tables it WITH (NOLOCK) ON p.object_id = it.object_id
+		`,
+		dataModels: &[]struct {
+			database.DataModel
+			ReservedSpace        float64 `db:"reserved_space" metric_name:"pageFileTotal" source_type:"gauge"`
+			ReservedSpaceNotUsed float64 `db:"reserved_space_not_used" metric_name:"pageFileAvailable" source_type:"gauge"`
+		}{},
+	},
+}
+
 var specificDatabaseDefinitions = []*QueryDefinition{
 	{
 		query: fmt.Sprintf(`USE "%s"
